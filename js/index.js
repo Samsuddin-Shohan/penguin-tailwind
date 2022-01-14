efb0a9c641f6cd6d74fa6c68a088486c95b699da
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-01-15 01:22:30 +0600

    js added

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const headerButton = document.getElementById('header-button');
+headerButton.addEventListener('click', ()=>{
+    document.getElementById('header-first-portion').innerText = 'i am the penguin';
+})
